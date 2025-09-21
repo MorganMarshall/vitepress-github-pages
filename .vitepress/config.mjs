2b552086b4ec30c818e5ad4fbbdd84fd68f80d8a
Morgan Marshall <imorganmarshall@gmail.com>
2025-09-21 11:07:29 -0400

    Set base path for VitePress configuration

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress-github-pages/',
   title: "VitePress Github Pages",
   description: "Example",
   themeConfig: {
